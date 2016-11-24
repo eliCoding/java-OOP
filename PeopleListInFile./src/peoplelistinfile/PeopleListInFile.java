491b586c5c1a67f5721d24197954b921fd9f2a09
Elmira <Elmira@10.64.39.236>
2016-11-23 19:34:10 -0500

    PeopleListInFile

--- PeopleListInFile./src/peoplelistinfile/PeopleListInFile.java
+++ PeopleListInFile./src/peoplelistinfile/PeopleListInFile.java
@@ -1,3 +1,127 @@
+/*
+HOMEWORK - day 2
+
+Create project PeopleListInFile
+
+You will display a menu to the user:
+1. Add person info
+2. List persons info
+3. Find a person by name
+4. Find all persons younger than age
+0. Exit
+
+You will implement functions to manage contents of file "people.txt".
+
+Example session
+
+What do you want to do?
+1. Add person info
+2. List persons info
+3. Find and list a person by name
+4. Find and list persons younger than age
+0. Exit
+Choice: 1
+
+Adding a person.
+Enter name: Jerry
+Enter age: 22
+Enter city: Montreal
+Person added.
+
+What do you want to do?
+1. Add person info
+2. List persons info
+3. Find a person by name
+4. Find all persons younger than age
+0. Exit
+Choice: 1
+
+Adding a person.
+Enter name: Maria
+Enter age: 26
+Enter city: Toronto
+Person added.
+
+What do you want to do?
+1. Add person info
+2. List persons info
+3. Find a person by name
+4. Find all persons younger than age
+0. Exit
+Choice: 2
+
+Listing all persons
+Jerry is 22 from Montreal
+Maria is 26 from Toronto
+
+What do you want to do?
+1. Add person info
+2. List persons info
+3. Find a person by name
+4. Find all persons younger than age
+0. Exit
+Choice: 3
+
+Enter partial person name:
+ri
+Matches found:
+Maria is 26 from Toronto
+
+What do you want to do?
+1. Add person info
+2. List persons info
+3. Find a person by name
+4. Find all persons younger than age
+0. Exit
+Choice: 4
+
+Enter maximum age:
+25
+Matches found:
+Jerry is 22 from Montreal
+
+What do you want to do?
+1. Add person info
+2. List persons info
+3. Find a person by name
+4. Find all persons younger than age
+0. Exit
+Choice: 0
+
+Good bye!
+
+
+
+Result data file will have the following contents.
+Jerry
+22
+Montreal
+Maria
+26
+Toronto
+
+
+NOTE: If you run your program again you will operate
+on data you already have, example:
+
+What do you want to do?
+1. Add person info
+2. List persons info
+3. Find a person by name
+4. Find all persons younger than age
+0. Exit
+Choice: 2
+
+Listing all persons
+Jerry is 22 from Montreal
+Maria is 26 from Toronto
+
+
+NOTE: You do NOT need to and are not allowed to
+store data in memory in array or ArrayList.
+You must read file every time.
+
+ */
 package peoplelistinfile;
 
 import java.io.File;
@@ -19,18 +143,17 @@ public class PeopleListInFile {
                 System.out.println("Please Enter Your Choice:");
                 int choice = input.nextInt();
                 System.out.println("Your Choice is:" + choice);
-                
-                 if ((choice < 0) || (choice > 5)) {
-                System.out.println("Invalid choice");
-                continue;
-            }
-            if (choice == 0) {
-                System.out.println("Good bye");
-                break;
-            }
+
+                if ((choice < 0) || (choice > 5)) {
+                    System.out.println("Invalid choice");
+                    continue;
+                }
+                if (choice == 0) {
+                    System.out.println("Good bye");
+                    break;
+                }
                 switch (choice) {
-                   
-                        
+
                     case 1:
                         Case1();
                         break;
@@ -41,9 +164,6 @@ public class PeopleListInFile {
 
                     case 3:
                         System.out.println(" Enter partial person name:");
-                        
-                        
-                        
 
                 }
             }
